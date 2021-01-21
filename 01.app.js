d2f734cee6e6a80c9623d7e9063efdd9924ccaa4
booldook <booldook@gmail.com>
2021-01-21 15:25:28 +0900

    ....

--- 01.app.js
+++ 01.app.js
@@ -4,17 +4,22 @@ const path = require('path');
 const moment = require('moment');
 const { mysql, connection } = require('./modules/mysql-conn');
 
-app.listen(3000, () => { console.log('//127.0.0.1:3000'); });
+app.listen(3000, () => { console.log('http://127.0.0.1:3000'); });
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.locals.pretty = true;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+// 도시 리스트
 app.get('/city', (req, res) => {
-	res.send('/city');
+	const pug = {
+		file: 'city',
+	}
+	res.render('city/list', pug);
 });
 
+// 도시 등록
 app.get('/city/create', (req, res) => {
 	res.send('/city/create');
 });
