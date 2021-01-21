bd2ce08ad372df246147881a4bc8768a87d9b285
booldook <booldook@gmail.com>
2021-01-21 16:28:07 +0900

    ....

--- routes/city-route.js
+++ routes/city-route.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { connection } = require('../modules/mysql-conn');
+const numeral = require('numeral');
 
 // 도시 리스트
 router.get('/', (req, res) => {
-	const pug = {
-		file: 'city',
+	const sql = 'SELECT * FROM city ORDER BY name ASC';
+	const onQuery = (err, r) => {
+		for(let v of r) {
+			v.population = numeral(v.population).format('0,0')+'명';
+		}
+		res.render('city/list', { file: 'city', data: r });
 	}
-	res.render('city/list', pug);
+	connection.query(sql, onQuery);
 });
 
 // 도시 등록
