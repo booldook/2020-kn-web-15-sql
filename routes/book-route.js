02389c0d9fdfeac6d666d57dc2504c8ac379e4d5
booldook <booldook@gmail.com>
2021-01-25 10:52:02 +0900

    ...

--- routes/book-route.js
+++ routes/book-route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const pug = { file: 'book', title: '도서등록시스템' }
 
-router.get('/write', (req, res) => {
+router.get('/create', (req, res) => {
 	const pug = { ...pug };
-	res.render('book/write', pug);
+	res.render('book/create', pug);
 });
 
 module.exports = router;
